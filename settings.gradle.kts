7ca9ac10713185545d8933394eb9e31d82fd9eb2
RamiroCecchetto <ramirocecchetto123gmail.com>
2023-08-01 16:03:22 -0300

    primer commit

---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "busca_minas"
+
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import com.cecchetto.BuscaMinas;
+
+public class Main {
+    public static void main(String[] args) {
+        new BuscaMinas();
+    }
+}
