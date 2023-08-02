b1d564ce3a2c8eb06f5a56b498652ee4aba400ff
RamiroCecchetto <ramirocecchetto123gmail.com>
2023-08-02 16:34:39 -0300

    se indica la adyacencia de cada celda

--- src/main/java/com/cecchetto/Cuadricula.java
+++ src/main/java/com/cecchetto/Cuadricula.java
@@ -8,13 +8,28 @@ public class Cuadricula {
 
     JButton btn;
     BufferedImage imgBomba;
+    Boolean mina;
+    Integer num = 0;
     public Cuadricula() {
         btn = new JButton();
         btn.setPreferredSize(new Dimension(50, 50));
 
         imgBomba = Util.getImage("mina.png");
+        mina = false;
     }
 
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public Boolean isMina() {
+        return mina;
+    }
+
+    public void setMina() {
+        btn.setBackground(Color.BLACK);
+        mina = true;
+    }
     public JButton getButton () {
         return btn;
     }
--- src/main/java/com/cecchetto/Tablero.java
+++ src/main/java/com/cecchetto/Tablero.java
@@ -31,27 +31,63 @@ public class Tablero extends JPanel{
             }
         }
 
-        asignarCuadriculas();
+        asugnasMinas();
+        asignarNumeros();
+
+    }
+
+    private void asignarNumeros() {
+
+        int[] dx = {-1, -1, -1, 0, 0, 1, 1, 1};
+        int[] dy = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+        for (int x = 0; x < dimension.width; x++) {
+            for (int y = 0; y < dimension.height; y++) {
+
+                if (!cuadraditos.get(x).get(y).isMina()) {
+                    int cantidadMinas = 0;
+                    for (int i = 0; i < 8; i++) {
+                        int x1 = x + dx[i];
+                        int y1 = y + dy[i];
+
+                        if (x1 >= 0 && x1 < dimension.width && y1 >= 0 && y1 < dimension.height)
+                            if (cuadraditos.get(x1).get(y1).isMina())
+                                cantidadMinas++;
+
+                    }
+                    cuadraditos.get(x).get(y).getButton().setText(Integer.toString(cantidadMinas));
+                }
+
+            }
+        }
+
+//        for (int x=0 ; x<dimension.width ; x++) {
+//            for (int y=0 ; y<dimension.height ; y++) {
+//
+//                if (!cuadraditos.get(x).get(y).isMina()) {
+//                    int num = 0;
+//                    for (int x1=x-1 ; x1<x)
+//                }
+//
+//            }
+//        }
 
     }
 
 
     int cantidadMinas;
-    private void asignarCuadriculas() {
+    private void asugnasMinas() {
         if (dimension.height == 8) cantidadMinas = 10;
         if (dimension.height == 16) cantidadMinas = 40;
         if (dimension.height == 30) cantidadMinas = 99;
 
         Set<Point> puntosAleatorios = new HashSet<>();
 
-
-        while (puntosAleatorios.size() < cantidadMinas) {
-
+        while (puntosAleatorios.size() < cantidadMinas)
             puntosAleatorios.add(new Point(new Random().nextInt(dimension.width), new Random().nextInt(dimension.height)));
 
-        }
-
-
+        for (Point puntosAleatorio : puntosAleatorios)
+            cuadraditos.get(puntosAleatorio.x).get(puntosAleatorio.y).setMina();
     }
 
 
